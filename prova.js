333376c59c33d6fa60d3679fbdbeaae6cf79f390
Lupennat <claudio.pennati@gmail.com>
2022-12-15 17:31:11 +0100

    safe-publish removed tmp file

--- prova.js
+++
@@ -1,7 +0,0 @@
-require('reflect-metadata');
-const { default: circularAStub } = require('./dist/cjs/__tests__/fixtures/circular-a-stub');
-const { default: circularBStub } = require('./dist/cjs/__tests__/fixtures/circular-b-stub');
-const { default: CircularCStub } = require("./dist/cjs/__tests__/fixtures/circular-c-stub");
-
-
-console.log( new CircularCStub(new circularAStub(new circularBStub())));
