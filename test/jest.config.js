cacf5637a43ac3ddd1e47d0a67dd6e0cf15f8556
Lupennat <claudio.pennati@gmail.com>
2022-12-17 17:05:13 +0100

    coverage cobertura

--- test/jest.config.js
+++ test/jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     rootDir: '..',
     clearMocks: true,
     coverageDirectory: 'coverage',
+    coverageReporters: ['html', 'text', 'text-summary', 'cobertura'],
     collectCoverageFrom: ['src/**/*.ts'],
     coveragePathIgnorePatterns: ['/node_modules/', 'types\\.ts', 'index\\.ts', '.+\\.d\\.ts'],
     testPathIgnorePatterns: ['/node_modules/', '/fixtures/', '/utils/'],
